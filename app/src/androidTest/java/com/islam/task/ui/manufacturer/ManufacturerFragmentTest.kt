3dd8aa980e1edf1dded3a666cc71ddbc5f1b2da3
islam.ar <islam.ahmed@approcks.com>
2021-07-09 22:16:12 +0200

    UI testing manufacturer list 2

--- app/src/androidTest/java/com/islam/task/ui/manufacturer/ManufacturerFragmentTest.kt
+++ app/src/androidTest/java/com/islam/task/ui/manufacturer/ManufacturerFragmentTest.kt
@@ -4,17 +4,21 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.codingwithmitch.espressouitestexamples.util.EspressoIdlingResourceRule
 import com.islam.task.R
 import com.islam.task.data.entity.ItemModel
+import com.islam.task.ui.MainActivity
 import com.islam.task.ui.adapters.ManufacturerAdapter
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -22,7 +26,22 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ManufacturerFragmentTest {
 
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get: Rule
+    val espressoIdlingResoureRule = EspressoIdlingResourceRule()
+
     private lateinit var scenario: FragmentScenario<ManufacturerFragment>
+
+    private val recyclerView: Int = R.id.list
+
+    val Item_Postition = 8
+    val items = listOf(
+        ItemModel(key = "107", value = "Bentley"),
+        ItemModel(key = "130", value = "BMW"),
+        ItemModel(key = "125", value = "Borgward")
+    )
 
     @Before
     fun setUp() {
@@ -33,8 +52,28 @@ class ManufacturerFragmentTest {
     @Test
     fun checkIfListIsVisible_onAppLaunch() {
 
-        onView(withId(R.id.list)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(this.recyclerView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
+    }
+
+    @Test
+    fun test_selectListItem_InList() {
+
+
+        onView(withId(this.recyclerView)).perform(
+            scrollToPosition<ManufacturerAdapter.ViewHolder>(
+                Item_Postition
+            )
+        )
+        /*    onView(withId(this.recyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<ManufacturerAdapter.ViewHolder>(
+                    Item_Postition,
+                    click()
+                )
+            )
+
+             onView(withId(R.id.toolbar))
+                 .check(ViewAssertions.matches(withText("Car Types >> BMW")))*/
     }
 
 
